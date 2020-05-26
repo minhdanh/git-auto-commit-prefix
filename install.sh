37df82755e9516492e258608a25a6ea82976a1cb
minhdanh <minhdanh@users.noreply.github.com>
2020-05-26 13:32:30 +0700

    Fix typos

--- install.sh
+++ install.sh
@@ -3,11 +3,11 @@
 echo "INFO Creating ~/.git-hooks directory"
 mkdir -p $HOME/.git-hooks
 
-echo "INFRO Download prepare-commit-msg"
+echo "INFO Download prepare-commit-msg"
 curl -sL https://raw.githubusercontent.com/minhdanh/git-auto-commit-prefix/master/prepare-commit-msg -o $HOME/.git-hooks/prepare-commit-msg
 chmod +x $HOME/.git-hooks/prepare-commit-msg
 
 echo "INFO Set core.hooksPath to $HOME/.git-hooks"
 git config --global core.hooksPath $HOME/.git-hooks
 
-echo "INFRO Done"
+echo "INFO Done"
